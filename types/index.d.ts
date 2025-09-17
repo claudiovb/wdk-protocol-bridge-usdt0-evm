808da413622e32210089b55c87d8f8806e5cf43d
Davide Casale <davi0kprogramsthings@gmail.com>
2025-09-17 16:46:36 +0200

    Fix types.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
 export { default } from "./src/usdt0-protocol-evm.js";
 export type BridgeProtocolConfig = import("@wdk/wallet/protocols").BridgeProtocolConfig;
 export type BridgeOptions = import("@wdk/wallet/protocols").BridgeOptions;
-export type Usdt0BridgeResult = import("./src/usdt0-protocol-evm.js").Usdt0BridgeResult;
+export type BridgeResult = import("./src/usdt0-protocol-evm.js").BridgeResult;
